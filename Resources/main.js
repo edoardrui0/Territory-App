11168d57a83992f00672292f916769b0fb9615d4
Edoardo Ruiz <edoardrui0@gmail.com>
2020-06-26 21:55:52 -0400

    Back-Up

---
+++ Resources/main.js
@@ -0,0 +1,165 @@
+function goBack() {
+    window.history.back();
+  }
+  
+// console.log('Hello There');
+// console.log('General Kenobi!');
+
+
+// let today = new Date();
+// let dd = String(today.getDate()).padStart(2, '0');
+// let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+// let yyyy = today.getFullYear();
+
+// today = mm + '/' + dd + '/' + yyyy;
+// console.log(today);
+
+// let random = Math.floor(Math.random() * 7) + 1;
+// console.log(random);
+
+// let data = {};
+// let storedData = JSON.parse(localStorage.getItem('data'));
+// if (storedData) {
+//   data = storedData;
+// } else {
+//   data.firstName = prompt('What is your first name?');
+//   data.lastName = prompt('What is your last name?');
+// }
+// localStorage.setItem('data', JSON.stringify(data))
+// alert(`Hello ${data.firstName} ${data.lastName}!`);
+
+
+
+
+
+const addresses = {
+  "territory-2": [
+    {
+      "page": "5",
+      "zipCode": "33125",
+      "streetOrAvenue": "NW 19 Ave",
+      "houseOrBuildingNum": [
+        {
+          "houseNum": "1843",
+          "records": [
+              {
+              "date": "06-23-2020",
+              "mark": null,
+              "notes": ""
+            }
+          ],
+          "houseNum": "1845",
+          "records": [
+            {
+              "date": "06-23-2020",
+              "mark": null,
+              "notes": ""
+            }
+          ],
+          "houseNum": "1847",
+          "records": [
+            {
+              "date": "06-23-2020",
+              "mark": null,
+              "notes": ""
+            }
+          ],
+          "houseNum": '1881',
+          "records": [
+            {
+              "date": "06-23-2020",
+              "mark": null,
+              "notes": ""
+            }
+          ] 
+        }
+      ]
+    },
+    {
+      "page": "9",
+      "zipCode": "33125",
+      "streetOrAvenue": "NW 18 Terrace",
+      "houseOrBuildingNum": [
+        { 
+          "buildingNum": "1753",
+          "aptNum": [
+            {
+              "number": "1",
+              "records": [
+                {
+                  "date": "06-23-2020",
+                  "mark": null,
+                  "notes": ""
+                }
+              ],
+              "number": "2",
+              "records": [
+                {
+                  "date": "06-23-2020",
+                  "mark": null,
+                  "notes": ""
+                }
+              ],
+              "number": "3",
+              "records": [
+                {
+                  "date": "06-23-2020",
+                  "mark": null,
+                  "notes": ""
+                }
+              ],
+              "number": "4",
+              "records": [
+                {
+                  "date": "06-23-2020",
+                  "mark": null,
+                  "notes": ""
+                }
+              ],
+              "number": "5",
+              "records": [
+                {
+                  "date": "06-23-2020",
+                  "mark": null,
+                  "notes": ""
+                }
+              ],
+              "number": "6",
+              "records": [
+                {
+                  "date": "06-23-2020",
+                  "mark": null,
+                  "notes": ""
+                }
+              ],
+              "number": "7",
+              "records": [
+                {
+                  "date": "06-23-2020",
+                  "mark": null,
+                  "notes": ""
+                }
+              ],
+              "number": "8",
+              "records": [
+                {
+                  "date": "06-23-2020",
+                  "mark": null,
+                  "notes": ""
+                }
+              ],
+              "number": "9",
+              "records": [
+                {
+                  "date": "06-23-2020",
+                  "mark": null,
+                  "notes": ""
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
